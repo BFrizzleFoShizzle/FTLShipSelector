d31da645fc4b75dfd52c47ada348475a816e33a8
Bryden Frizzell <bryden.frizzell@gmail.com>
2015-09-02 10:27:35 +1200

    Deleted unnessesary files

--- FTLShipSelector.h
+++ FTLShipSelector.h
@@ -3,12 +3,8 @@
 #include <windows.h>
 #include <cstdio>
 #include <gl\GL.h>
-#include "FTLShip.h"
 #include "DirtyHooker.h"
 #include "ShipDescriptor.h"
-
-ship* ships = NULL;
-ship* playerShip = NULL;
 
 void readBitmapFont(FILE* file);
 void drawChar(float x, float y, char text);
